a43b2eeae3ae3ddc1f1a4e472c274f03dd4fc170
chihming <changecandy@gmail.com>
2016-11-24 17:16:32 +0800

    smaller batch for small network

--- deepwalk/skipgram.py
+++ deepwalk/skipgram.py
@@ -21,6 +21,7 @@ class Skipgram(Word2Vec):
         kwargs["size"] = kwargs.get("size", 128)
         kwargs["sentences"] = kwargs.get("sentences", None)
         kwargs["sg"] = kwargs.get("sg", 0)
+        kwargs["batch_words"] = 100
 
         if vocabulary_counts != None:
           self.vocabulary_counts = vocabulary_counts
